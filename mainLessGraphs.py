0ad28afe7363a6159c871cf51d0f72094343737f
CadenSantopadre <csantopadre26@gmail.com>
2026-09-20 19:38:35 -0700

    Added more data

--- mainLessGraphs.py
+++ mainLessGraphs.py
@@ -2,7 +2,6 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from scipy import stats
-from statsmodels.graphics.tsaplots import plot_acf
 import statsmodels.api as sm
 
 df = pd.read_csv("study.csv")
